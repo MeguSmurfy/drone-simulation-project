78afd48fdf5688116e5f15e9ab2e63ae157b2fe4
findo009 <findo009@umn.edu>
2023-12-15 16:15:40 -0600

    DroneObserver.h Doxygen

--- libs/transit/include/DroneObserver.h
+++ libs/transit/include/DroneObserver.h
@@ -1,3 +1,7 @@
+/**
+ * @file DroneObserver.h
+ * @brief Header file for the DroneObserver class.
+ */
 #ifndef DRONE_OBSERVER_H_
 #define DRONE_OBSERVER_H_
 
@@ -5,11 +9,47 @@
 #include "Drone.h"
 #include "Package.h"
 
+/**
+ * @class DroneObserver
+ * @brief Observer class for monitoring drone state changes.
+ *
+ * DroneObserver implements the observer pattern and is used to track the state
+ * and behavior of a Drone object. It updates its state based on the changes in the
+ * observed Drone.
+ */
 class DroneObserver: public IObserver {
  public:
+   /**
+   * @brief Constructs a DroneObserver object.
+   * 
+   * Initializes a new instance of the DroneObserver class, setting the target
+   * Drone object to observe.
+   *
+   * @param drone Pointer to the Drone object to observe.
+   */
   DroneObserver(Drone* drone);
+ 
+  /**
+   * @brief Destructor for DroneObserver.
+   */
   ~DroneObserver() {}
+ 
+  /**
+   * @brief Updates the observer based on the observed Drone's state.
+   * 
+   * Called to update the state of the DroneObserver in response to changes
+   * in the observed Drone's state.
+   *
+   * @param dt Time delta for updating observer's state.
+   * @return std::string A summary of the current state of the observer.
+   */
   std::string Update(double dt);
+ 
+  /**
+   * @brief Retrieves the ID of the observed Drone.
+   * 
+   * @return int The ID of the Drone being observed.
+   */
   int GetId();
  private:
   Drone* drone;
