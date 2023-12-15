4f7124593cdf02e7a80854e43a599f5dcb1ca1ca
findo009 <findo009@umn.edu>
2023-12-15 15:50:37 -0600

    AstarStrategy.h Doxygen Comments

--- libs/transit/src/AstarStrategy.cc
+++ libs/transit/src/AstarStrategy.cc
@@ -1,5 +1,30 @@
+/**
+ * @file AstarStrategy.h
+ * @brief This file contains the AstarStrategy class which implements the A* algorithm for pathfinding.
+ */
+
 #include "AstarStrategy.h"
 #include "routing/astar.h"
+
+/**
+ * @class AstarStrategy
+ * @brief Implements a strategy for pathfinding using the A* algorithm.
+ * 
+ * AstarStrategy is a class that uses the A* algorithm to find a path from a start
+ * position to a destination in a graph. It is designed to work with
+ * the routing::IGraph interface for obtaining the path.
+ */
+
+/**
+ * @brief Creates an AstarStrategy object.
+ * 
+ * This constructor initializes the AstarStrategy object with a starting position, a 
+ * destination, and a graph on which the A* algorithm will be executed.
+ * 
+ * @param pos The starting position as a Vector3.
+ * @param des The destination position as a Vector3.
+ * @param g Pointer to the graph on which A* algorithm will be used.
+ */
 
 AstarStrategy::AstarStrategy(Vector3 pos, Vector3 des,
                              const routing::IGraph* g) {
